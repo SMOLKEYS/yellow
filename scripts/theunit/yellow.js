ea39350f2a9c33fdd401686f94cbd95502273604
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-08 11:19:28 +0800

    begonent

--- scripts/theunit/yellow.js
+++ scripts/theunit/yellow.js
@@ -5,10 +5,9 @@ const yellow = extend(UnitType, "yellow", {
   accel: 0.08,
   drag: 0.01,
   flying: true,
-  sprite: Core.atlas.find("flare"),
-  defaultController: () => extend(DefenderAI, {}),
 });
 //aaaaaaa
 yellow.constructor = () => extend(UnitEntity, {});
+Vars.content.unit(39).defaultController = () => new DefenderAI();
 
 Log.info("yellow");
