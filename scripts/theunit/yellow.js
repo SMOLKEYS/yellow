fa14678732d86dbf2c5fb1c722380d556db06722
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-08 08:39:04 +0800

    update yellow.js

--- scripts/theunit/yellow.js
+++ scripts/theunit/yellow.js
@@ -1,11 +1,11 @@
 //hopefully this works
 const yellow = extend(UnitType, "yellow", {
-//health is intentional, as yellow is literally just gonna be reginald from gray
-  health: Number.MAX_VALUE;
-  speed: 3;
-  accel: 0.08;
-  drag: 0.01;
-  flying: true;
-  sprite: flare;
-  defaultController: new DefenderAI;
+  health: Number.MAX_VALUE,
+  type: flying,
+  speed: 3,
+  accel: 0.08,
+  drag: 0.01,
+  flying: true,
+  sprite: flare,
+  defaultController: new DefenderAI(),
 });
