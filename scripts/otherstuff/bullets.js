eac47e932e378bcc77bb73a9536a03ac8490aefb
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-14 17:38:08 +0800

    hopes

--- scripts/otherstuff/bullets.js
+++ scripts/otherstuff/bullets.js
@@ -1,1 +1,11 @@
-//has nothing, yet
+
+const yellowMelterWeaponBullet = extend(ContinuousLaserBulletType, {
+  damage: 40,
+  length: 8,
+  width: 30 * 8,
+  lifetime: 60,
+  colors: [
+    Pal.accent,
+    Pal.accent,
+    ]
+});
