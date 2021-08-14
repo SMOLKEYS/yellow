e52a116fae585d4e59671bbe6e5d21bb3fef13ff
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-14 20:43:58 +0800

    argehsy

--- scripts/theunit/yellow.js
+++ scripts/theunit/yellow.js
@@ -37,8 +37,8 @@ const yellowDripShoes = extend(Weapon, {
 const YDSB = extend(BasicBulletType, {
   damage: 10,
   knockback: 10,
-  length: 8 * 12,
-  width: 8 * 8,
+  length: 8 * 8,
+  width: 8 * 4,
   lifetime: 60 * 5,
   sprite: "yellow-yellowShoes",
 });
