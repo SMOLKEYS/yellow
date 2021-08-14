711bcf4e46810e601a4acac441540300d686fc94
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-14 18:04:15 +0800

    abababababab

--- scripts/theunit/yellow.js
+++ scripts/theunit/yellow.js
@@ -8,6 +8,8 @@ const yellow = extend(UnitType, "yellow", {
   speed: 3,
   accel: 0.08,
   drag: 0.01,
+  range: 240,
+  maxRange: 240,
   flying: true,
 });
 //required, for some reason(thanks to iarkn#8872 for helping with this)
