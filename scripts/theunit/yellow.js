357c815fb5af15065366c24e0a6a8eaf2c23aab6
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-14 19:08:16 +0800

    beats me on what im trying here

--- scripts/theunit/yellow.js
+++ scripts/theunit/yellow.js
@@ -14,7 +14,7 @@ const YMWB = extend(ContinuousLaserBulletType, {
 });
 
 const yellowDropkick = extend(Weapon, {
-  recoil: -80,
+  recoil: -14,
   reload: 60 * 2,
 });
 
@@ -23,6 +23,7 @@ const YDB = extend(LaserBulletType, {
   length: 8 * 8,
   width: 1,
   lifetime: 1,
+  recoil: -14,
 });
 
 yellowMelterWeapon.bullet = YMWB;
