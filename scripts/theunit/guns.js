4e0c4c94addda6a04ab5c5a23f71df5f4a83fb42
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-14 18:06:12 +0800

    ugh

--- scripts/theunit/guns.js
+++ scripts/theunit/guns.js
@@ -1,13 +1,1 @@
 
-const yellowMelterWeapon = extend(Weapon, {
-  reload: 120,
-});
-
-const YMWB = extend(ContinuousLaserBulletType, {
-  damage: 40,
-  length: 8,
-  width: 240,
-  lifetime: 60,
-});
-
-yellowMelterWeapon.bullet = YMWB;
