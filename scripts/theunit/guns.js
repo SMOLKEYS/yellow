38e1838cac2d14a721a086df78729ae4c4f52590
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-14 17:57:22 +0800

    Create guns.js

---
+++ scripts/theunit/guns.js
@@ -0,0 +1,13 @@
+
+const yellowMelterWeapon = extend(Weapon, {
+  reload: 2 * 60,
+});
+
+const YMWB = extend(ContinuousLaserBulletType, {
+  damage: 40,
+  length: 8,
+  width: 30 * 8,
+  lifetime: 60,
+});
+
+yellowMelterWeapon.bullet = YMWB;
