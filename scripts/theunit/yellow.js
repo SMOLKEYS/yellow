fbca63d4854532b57d7383cb4dc072dac6f5917b
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-15 12:41:49 +0800

    seperation for better managing

--- scripts/theunit/yellow.js
+++ scripts/theunit/yellow.js
@@ -1,34 +1,5 @@
 
-const yellowMelterWeapon = extend(Weapon, {
-  reload: 60,
-  x: 8 * 7,
-  shots: 15,
-  inaccuracy: 30,
-  shootSound: Sounds.explosionbig,
-});
-
-const yellowMelterWeaponBullet = extend(ContinuousLaserBulletType, {
-  damage: 75 /* begone */,
-  length: 30 * 8,
-  width: 8,
-  lifetime: 60,
-});
-
-const yellowBloom = extend(Weapon, {
-  reload: 60 * 5,
-  shots: 500,
-  inaccuracy: 360,
-  burstSpacing: 10,
-});
-
-const yellowBloomBullet = extend(BasicBulletType, {
-  damage: 50,
-  lifetime: 60 * 3,
-  speed: 4,
-});
-
-yellowMelterWeapon.bullet = yellowMelterWeaponBullet;
-yellowBloom.bullet = yellowBloomBullet;
+const yellowGuns = require("guns");
 
 const yellow = extend(UnitType, "yellow", {
   health: Number.MAX_VALUE,
@@ -45,7 +16,8 @@ yellow.constructor = () => extend(UnitEntity, {});
 yellow.defaultController = () => extend(DefenderAI, {});
 //weaponry
 yellow.weapons.add(
-  yellowMelterWeapon, yellowBloom
+  yellowGuns.yellowMelterWeapon,
+  yellowGuns.yellowBloom
 );
 
 Log.info("yellow");
