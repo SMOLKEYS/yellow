8993190c5e08f2ee899962b4ccdb4a643819f0a5
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-15 12:47:28 +0800

    spacing (please work)

--- scripts/theunit/weaponry.js
+++ scripts/theunit/weaponry.js
@@ -18,7 +18,7 @@ const yellowBloom = extend(Weapon, {
   reload: 60 * 5,
   shots: 500,
   inaccuracy: 360,
-  burstSpacing: 10,
+  spacing: 5,
 });
 
 const yellowBloomBullet = extend(BasicBulletType, {
