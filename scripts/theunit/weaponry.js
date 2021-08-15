6bee7f5562b95ec98bfd5694aff9e1e6566b5960
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-15 13:23:52 +0800

    haha

--- scripts/theunit/weaponry.js
+++ scripts/theunit/weaponry.js
@@ -18,7 +18,7 @@ const yellowBloom = extend(Weapon, {
   reload: 60 * 5,
   shots: 500,
   inaccuracy: 360,
-  spacing: 5,
+  shotDelay: 5,
 });
 
 const yellowBloomBullet = extend(BasicBulletType, {
