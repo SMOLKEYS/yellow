64ebd24037ab0f71b04cb7fea0b25aadc7a98d8b
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-15 12:40:01 +0800

    Create weaponry.js

---
+++ scripts/theunit/weaponry.js
@@ -0,0 +1,31 @@
+
+const yellowMelterWeapon = extend(Weapon, {
+  reload: 60,
+  x: 8 * 7,
+  shots: 15,
+  inaccuracy: 30,
+  shootSound: Sounds.explosionbig,
+});
+
+const yellowMelterWeaponBullet = extend(ContinuousLaserBulletType, {
+  damage: 75 /* begone */,
+  length: 30 * 8,
+  width: 8,
+  lifetime: 60,
+});
+
+const yellowBloom = extend(Weapon, {
+  reload: 60 * 5,
+  shots: 500,
+  inaccuracy: 360,
+  burstSpacing: 10,
+});
+
+const yellowBloomBullet = extend(BasicBulletType, {
+  damage: 50,
+  lifetime: 60 * 3,
+  speed: 4,
+});
+
+yellowMelterWeapon.bullet = yellowMelterWeaponBullet;
+yellowBloom.bullet = yellowBloomBullet;
