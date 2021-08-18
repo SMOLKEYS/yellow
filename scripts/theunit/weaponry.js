c2b66f3471444ff047b5db68396376ab476a03fc
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-08-18 12:32:47 +0800

    renames

--- scripts/theunit/weaponry.js
+++ scripts/theunit/weaponry.js
@@ -1,5 +1,5 @@
 
-const yellowMelterWeapon = extend(Weapon, {
+const meltdownShotgun = extend(Weapon, {
   reload: 60,
   x: 8 * 7,
   shots: 15,
@@ -7,52 +7,36 @@ const yellowMelterWeapon = extend(Weapon, {
   shootSound: Sounds.explosionbig,
 });
 
-const yellowMelterWeaponBullet = extend(ContinuousLaserBulletType, {
+const meltdownShotgunBullet = extend(ContinuousLaserBulletType, {
   damage: 80 /* begone */,
   width: 8 * 1,
   length: 30 * 8,
   lifetime: 60,
 });
 
-const yellowBloom = extend(Weapon, {
+const lucidity = extend(Weapon, {
   reload: 60 * 13,
   shots: 500,
   inaccuracy: 360,
   shotDelay: 1 /* angery */,
 });
 
-const yellowBloomBullet = extend(BasicBulletType, {
-  damage: 50,
+const lucidityBullet = extend(BasicBulletType, {
+  damage: 70,
   width: 8 * 8,
   height: 8 * 8,
   lifetime: 60 * 6,
   speed: 2,
+  color: [
+    ff0000,
+    ff0000
+    ],
 });
 
-const yellowCursedDuo = extend(Weapon, {
-  reload: 60 * 1,
-  shots: 69,
-  inaccuracy: 30,
-  rotate: true,
-  x: 8 * 6,
-  y: 8 * 6,
-  sprite: "duo",
-});
-
-const yellowCursedDuoBullet = extend(BasicBulletType, {
-  damage: 25,
-  width: 8 * 1,
-  height: 8 * 1,
-  speed: 5,
-  lifetime: 60 * 2,
-});
-
-yellowMelterWeapon.bullet = yellowMelterWeaponBullet;
-yellowBloom.bullet = yellowBloomBullet;
-yellowCursedDuo.bullet = yellowCursedDuoBullet;
+meltdownShotgun.bullet = meltdownShotgunBullet;
+lucidity.bullet = lucidityBullet;
 
 module.exports = {
-  yellowMelterWeapon: yellowMelterWeapon,
-  yellowBloom: yellowBloom,
-  yellowCursedDuo: yellowCursedDuo,
+  meltdownShotgun: meltdownShotgun,
+  lucidity: lucidity,
 };
